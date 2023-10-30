3812fdec9ee071c8cb2a9a4936386feaa191e920
victor_rxph <v.rxphdev@gmail.com>
2023-10-30 17:40:00 -0300

    Add functions to add and remove tasks

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ function setTheme() {
 let themeBtn = document.querySelector('#theme-btn');
 
 themeBtn.addEventListener('click', setTheme);
+
+let taskInput = document.querySelector('.todo-input');
+
+let taskInputValue;
+
+let uncompletedContainer = document.querySelector('.uncompleted-container');
+
+let todoItem = document.querySelector('.todo-item');
+
+taskInput.addEventListener('keydown', (e) => {
+
+    if (e.key === 'Enter'){
+
+        console.log('fds');
+        
+        let clonedTodo = todoItem.cloneNode(true);
+
+        let todoLabel = clonedTodo.querySelector('.todo-label')
+
+        todoLabel.textContent = taskInput.value;
+
+        uncompletedContainer.appendChild(clonedTodo);
+
+        taskInput.value = '';
+    }
+
+});
+
+let closeX = document.querySelector('.close-x');
+
+closeX.addEventListener('click', removeTodo);
+
+function removeTodo(e) {
+
+    console.log(e.target.parentNode);
+
+    e.preventDefault();
+
+    let parent = e.target.parentNode;
+
+    parent.remove();
+
+};
